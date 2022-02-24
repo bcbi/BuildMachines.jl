9a66e3ad0976ce02c56bd395c91a71f13f3c4a7c
Documenter.jl <documenter@juliadocs.github.io>
2022-02-24 15:58:44 +0000

    build based on cf017a5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BuildMachines","category":"page"},{"location":"#BuildMachines","page":"Home","title":"BuildMachines","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BuildMachines.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BuildMachines]","category":"page"}]
+}
